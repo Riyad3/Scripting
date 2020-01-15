66a6d583745ce383ae10e5651cf0248ac3861a39
Unknown <riyad.achour@edu.itescia.fr>
2020-01-15 17:24:32 +0100

    story 4 fini !

--- genSensorData.c
+++ genSensorData.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <string.h>
 
 char c;
@@ -7,16 +8,35 @@ char b;
 char isGood [100];
 char isErr [100];
 
-int main (int argc , char* argv[]) {
-	printf("Bienvenue\n");
-	b = '_';
+int main(int argc , char* argv[]){
+
 	while(read (0,&c,1)==1){
+char* sensorNames[]={
+"table",
+"chaise",
+"tabouret"
+};
+
+char* errorCodes[]={
+"Erreur 404",
+"Erreur 503",
+"Erreur 666"
+};
+int sensorId = rand()%(1000);
+char* sensorName = sensorNames[rand()%(3)];
+int minValue = rand()%(100 +1);
+int value = minValue + rand() %(100 +1);
+int meanValue = value + rand() %(100 + 1);
+int maxValue = meanValue + rand() %(100 + 1);
+char* errorCode = errorCodes[rand()%(3)];
+char errorDetail[] = "Une erreur est survenue !\n"; 
+ 
 		if (c==79 && b==10){
-			sprintf(isGood , "c'est good je suis une %d \n", c);
+			sprintf(isGood , "\n %d, %s, %d, %d, %d,%d \n", sensorId, sensorName,value, minValue, meanValue, maxValue );
 			write(1,isGood,strlen(isGood));	
 		}
 		else if(c==69 && b==10){
-			sprintf(isErr, "c'est une erreur je suis une %d \n",c);
+			sprintf(isErr, "\n %d, %s, %s \n",sensorId, errorCode, errorDetail);
 			write(2,isErr,strlen(isErr));
 		}
 	b = c;
