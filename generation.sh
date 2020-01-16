9dcf86d937cc76a2802dc4b43118b2ada8bb1c76
Unknown <riyad.achour@edu.itescia.fr>
2020-01-16 10:10:40 +0100

    story 5 done

--- generation.sh
+++ generation.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-./genTick $1 | ./genSensorData 2> $2 > $3
+
+if [ ! -d “$2” ]; then
+  mkdir -p  /Users/achourryad/Documents/github/Scripting/$2
+fi
+ ./genTick $1 | ./genSensorData 2>>$2/$3 >>$2/$4
